f51b31da70e8e6ca505e8886ea0943bb585b6fa7
ShiYiru97 <82817846+ShiYiru97@users.noreply.github.com>
2021-04-27 13:50:25 +0800

    add homework chapter1

---
+++ Homework/Chapter1/HomeworkC103.java
@@ -0,0 +1,6 @@
+//第一章作业 作业三 HomeworkC103
+//3.JDK,JRE,JVM的关系
+// 答：
+// 1.JDK = JRE + java开发工具[java，javac“java编译工具”，javadoc，javap等] = JVM + 核心类库；JDK java开发工具包。
+// 2.JRE = JVM + 核心类库[类]；JRE java运行环境， 包括java虚拟机和java程序所需要的核心库等。
+//    如果想要运行java程序，只要安装有JRE即可（运行.class文件，已编译的java文件）。
---
+++ Homework/Chapter1/HomeworkC104.java
@@ -0,0 +1,5 @@
+//HomeworkC104
+//环境变量path的配置及作用
+1.环境变量的作用是为了在dos的任意目录，都可以去使用java和javac命令；
+2.先配置JAVA_HOME = 指向jdk安装的主目录（上一级文件夹地址）；
+3.编辑path环境变量，增加%JAVA_HOME%\bin;
