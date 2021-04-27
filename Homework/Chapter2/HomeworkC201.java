db93bbf71f26c3873c1500775c6afefb9ff91e03
ShiYiru97 <82817846+ShiYiru97@users.noreply.github.com>
2021-04-27 13:54:12 +0800

    upload homework chapter2

---
+++ Homework/Chapter2/HomeworkC201.java
@@ -0,0 +1,16 @@
+//第二章作业 作业1
+//程序阅读题
+public class HomeworkC201{
+	public static void main(String[] args) {
+		int n1;
+		n1 = 13;
+		int n2;
+		n2 = 17;
+		int n3;
+		n3 = n1 + n2;//输出30
+		System.out.println("n3=" + n3);//输出 n3=30
+		int n4 = 38;
+		int n5 = n4 - n3;//8
+		System.out.println("n5=" + n5);//n5 = 8
+	}
+}
